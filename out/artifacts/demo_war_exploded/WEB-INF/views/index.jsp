695e4621db6109c8cb63aca257831f56e4ae4826
Vernon Williams <voxenn@gmail.com>
2018-10-12 22:27:12 -0500

    First commit.

---
+++ out/artifacts/demo_war_exploded/WEB-INF/views/index.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Voxenn
+  Date: 10/12/2018
+  Time: 9:38 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>${title}</title>
+    <p>${attribute}</p>
+  </head>
+  <body>
+  ${end}
+  </body>
+</html>
---
+++ src/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(Model m) {
+        m.addAttribute("attribute", "value");
+        m.addAttribute("title", "Demo Store");
+        m.addAttribute("end", "Take care!");
+        return "index";
+    }
+}
